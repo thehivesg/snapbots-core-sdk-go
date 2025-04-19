5fff4da454add14537265a966dccf06da9395a8c
Alex Sante <wre36316@adobe.com>
2025-04-19 18:58:48 +0000

    Add job package with NewJobRequest and JobResponse types, and Service for NATS communication

---
+++ pkg/job/job.go
@@ -0,0 +1,56 @@
+package job
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/nats-io/nats.go"
+)
+
+type NewJobRequest struct {
+	BotID      string `json:"bot_id"`
+	ConsumerID string `json:"consumer_id"`
+	RequestID  string `json:"request_id"`
+}
+
+type JobResponse struct {
+	ID            string        `json:"id"`
+	BotID         string        `json:"bot_id"`
+	ConsumerID    string        `json:"consumer_id"`
+	CompletedAt   time.Time     `json:"completed_at"`
+	Payload       interface{}   `json:"payload"`
+	RequestID     string        `json:"request_id"`
+	StartedAt     time.Time     `json:"started_at"`
+	Status        string        `json:"status"`
+	TotalDuration time.Duration `json:"total_duration"`
+	TotalBytes    int64         `json:"total_bytes"`
+}
+
+type Service struct {
+	natsClient *nats.Conn
+}
+
+func NewService(natsClient *nats.Conn) *Service {
+	return &Service{
+		natsClient: natsClient,
+	}
+}
+
+func (s *Service) NewJob(request NewJobRequest) (JobResponse, error) {
+	jobReqBytes, err := json.Marshal(request)
+	if err != nil {
+		return JobResponse{}, err
+	}
+
+	respBytes, err := s.natsClient.Request(request.BotID, jobReqBytes, 10*time.Second)
+	if err != nil {
+		return JobResponse{}, err
+	}
+
+	var resp JobResponse
+	if err := json.Unmarshal(respBytes.Data, &resp); err != nil {
+		return JobResponse{}, err
+	}
+
+	return resp, nil
+}
